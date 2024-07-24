92bfa61df59aa46f06771875cd95b4796ad124b0
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-07-24 12:48:08 +0200

    finish project

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,3 @@
-// Descrizione:
-// Rifare l'esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 const { createApp } = Vue
 
@@ -48,13 +36,28 @@ const { createApp } = Vue
 
             }
         },
-        remove(i){
-            this.todolist.splice( i , 1 )
-        },
-        addClass(i){
-            
-            this.todolist[i].done = !this.todolist[i].done
+        addClass(index) {
+            this.todolist[index].done = !this.todolist[index].done;
+          },
+          remove(index) {
+            this.todolist.splice(index, 1);
+          },
+          showModal() {
+            const modal = new bootstrap.Modal(document.getElementById('thankYouModal'));
+            modal.show();
+          } 
+    },
+    mounted() {
+    const flowerIcon = document.getElementById('flower-icon');
 
-        }
+    if (flowerIcon) {
+      flowerIcon.addEventListener('mouseover', () => {
+        flowerIcon.classList.add('petal-move');
+      });
+
+      flowerIcon.addEventListener('mouseout', () => {
+        flowerIcon.classList.remove('petal-move');
+      });
     }
-  }).mount('#app')
+  }
+}).mount('#app');
